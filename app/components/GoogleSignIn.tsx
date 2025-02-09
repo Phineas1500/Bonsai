161f5c0b450f3ff67b72e73645e9fe71031415c1
Leon <43097991+unkn-wn@users.noreply.github.com>
2025-02-09 16:57:14 -0500

    remove dotenv

--- app/components/GoogleSignIn.tsx
+++ app/components/GoogleSignIn.tsx
@@ -3,7 +3,6 @@ import * as Google from 'expo-auth-session/providers/google';
 import { Button } from 'react-native';
 import { useEffect, useState } from 'react';
 import SignInSuccess from './SignInSuccess';
-import { CLIENT_ID, IOS_CLIENT_ID, ANDROID_CLIENT_ID } from '@env';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -11,10 +10,10 @@ export default function GoogleSignIn() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: CLIENT_ID,
-    iosClientId: IOS_CLIENT_ID,
-    androidClientId: ANDROID_CLIENT_ID,
-    webClientId: CLIENT_ID,
+    clientId: process.env.EXPO_PUBLIC_CLIENT_ID,
+    iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+    androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+    webClientId: process.env.EXPO_PUBLIC_CLIENT_ID,
   });
 
   useEffect(() => {
