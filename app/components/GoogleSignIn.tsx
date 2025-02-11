cb2f465e6b538bf96e3fc77f58759930c251d2f5
michael <michaelforte11@gmail.com>
2025-02-11 15:18:27 -0500

    explicitly set redirect uri

--- app/components/GoogleSignIn.tsx
+++ app/components/GoogleSignIn.tsx
@@ -1,6 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import { Button } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
 import { useEffect, useState } from 'react';
 import SignInSuccess from './SignInSuccess';
 
@@ -9,11 +10,16 @@ WebBrowser.maybeCompleteAuthSession();
 export default function GoogleSignIn() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  const redirectUri = AuthSession.makeRedirectUri({
+    scheme: "com.phineas1500.bonsai",
+  })
+
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId: process.env.EXPO_PUBLIC_CLIENT_ID,
     iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
     androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
     webClientId: process.env.EXPO_PUBLIC_CLIENT_ID,
+    redirectUri
   });
 
   useEffect(() => {
