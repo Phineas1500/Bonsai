926b039e8f2b541e2c4df34ff8dfa4ab4671efec
michael <michaelforte11@gmail.com>
2025-02-24 18:08:56 -0500

    calendar sync and task list

---
+++ app/components/TaskListItem.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native"
+import { format } from 'date-fns';
+
+export interface TaskItemData {
+    id: string;
+    title: string;
+    description: string;
+    startTime: string;
+    endTime: string;
+}
+
+export default function TaskListItem(
+    { itemData }: { itemData: TaskItemData }
+) {
+
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <View className="m-1 rounded-md overflow-hidden">
+            <TouchableOpacity 
+                onPress={() => setExpanded(!expanded)}
+                className="p-2 bg-teal-800"
+            >
+                <Text className="text-white text-l font-bold">
+                    {itemData.title} - {format(new Date(itemData.startTime), 'EEE h:mm aaa')} 
+                </Text>
+            </TouchableOpacity>
+            {expanded ? 
+            <View className="bg-green-50 p-2 border border-black">
+                <Text className="font-bold text-black">Time:</Text>
+                <Text className="text-black mb-1">
+                    {format(new Date(itemData.startTime), 'PPP, h:mm aaa')} - {format(new Date(itemData.endTime), 'PPP, h:mm aaa')}
+
+                </Text>
+                <Text className="font-bold text-black">Description:</Text>
+                <Text className="text-black">
+                    {itemData.description}
+                </Text>
+            </View>
+            : null}
+            
+        </View>
+    );
+}
--- app/screens/tasks.tsx
+++ app/screens/tasks.tsx
@@ -1,9 +1,78 @@
-import { View, Text} from 'react-native';
-import { useState } from 'react';
+import { View, Text, ScrollView} from 'react-native';
+import { useEffect, useState } from 'react';
 import GradientButton from '@components/GradientButton';
 import Navbar from '../components/Navbar';
+import { useUser } from '../contexts/UserContext';
+import axios from "axios";
+import TaskListItem, { TaskItemData } from '../components/TaskListItem';
+import { format } from 'date-fns';
 
 export default function Tasks() {
+
+  const { userInfo, setUserInfo } = useUser();
+
+  const [taskData, setTaskData] = useState<TaskItemData[]>([]);
+
+  //fetch tasks from google calendar on page load
+  useEffect(() => {
+    fetchCalendarEvents(userInfo);
+  }, [])
+
+  const fetchCalendarEvents = async (userInfo: { username: string; email: string; usesGoogle: boolean; id_token?: string; googleAuth?: { access_token?: string; }; calendarAuth?: { access_token: string; refresh_token: string; }; } | null) => {
+    
+    console.log("USER INFO: ", userInfo);
+    if (!userInfo?.calendarAuth?.access_token) {
+        console.error("No access token available");
+        return;
+    }
+
+    try {
+        const response = await axios.get("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+            headers: {
+                Authorization: `Bearer ${userInfo.calendarAuth.access_token}`
+            },
+            params: {
+              timeMin: new Date().toISOString(),
+              maxResults: 10,
+              singleEvents: true,
+              orderBy: "startTime"
+            }
+        });
+        
+        //extract relavent data from response object
+        const newTaskList = [];
+        const taskItems = response.data.items;
+        for (const item of taskItems) {
+          const newTaskObj: TaskItemData = {
+            id: item.id,
+            title: item.summary,
+            description: item.description || "",
+            startTime: item.start.dateTime,
+            endTime: item.end.dateTime,
+            formattedTime: getFormattedTime(item.start.dateTime)
+          }
+          newTaskList.push(newTaskObj);
+        }
+        setTaskData(newTaskList);
+
+    } catch (error) {
+        console.error("Error fetching calendar events:", error);
+    }
+  };
+
+  function getFormattedTime(dateString: string): string {
+    // converts an ISO string into a Date and then formats it as "<day short name> <time> <AM or PM>"
+    return format(new Date(dateString), 'EEE. h:mm a');
+  }
+
+  const taskComponents = () => {
+    const components = taskData.map((taskItem: TaskItemData) => {
+      console.log(taskItem);
+      return <TaskListItem key={taskItem.id} itemData={taskItem}/>
+  });
+    console.log("Componenets:", components);
+    return components;
+  }
 
   return (
 
@@ -11,9 +80,12 @@ export default function Tasks() {
     <>
       <Navbar />
       <View className="flex-1 flex-col items-start bg-stone-950 p-6">
-          <Text className="text-sm font-light text-teal-500 text-center">
+          <Text className="text-xl font-light text-teal-500 text-center">
               Tasks:
           </Text>
+          <ScrollView>
+            {taskComponents()}
+          </ScrollView>
       </View>
     </>
   );
