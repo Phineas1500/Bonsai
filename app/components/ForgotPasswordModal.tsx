5c5ebcb934b0843c5e90f3cf5902ea2ea955a951
Caleb Chua <112199132+calebchua@users.noreply.github.com>
2025-02-26 01:05:00 -0500

    Forgot Password and Profile
    - Forgot password backend should work (not tested yet)
    - Profile page is styled

--- app/components/ForgotPasswordModal.tsx
+++ app/components/ForgotPasswordModal.tsx
@@ -1,6 +1,8 @@
-import { Modal, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Modal, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { useState } from 'react';
 import GradientButton from './GradientButton';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from 'firebaseConfig';
 
 interface ForgotPasswordModalProps {
   visible: boolean;
@@ -12,6 +14,17 @@ export default function ForgotPasswordModal({
   onRequestClose
 }: ForgotPasswordModalProps) {
   const [email, setEmail] = useState('')
+  const [requestSent, setRequestSent] = useState(false)
+
+  const resetPassword = async () => {
+    try {
+      await sendPasswordResetEmail(auth, email)
+        .then(() => setRequestSent(true))
+    } catch (err: any) {
+      Alert.alert('Error', 'Please enter a valid email');
+      console.log(err.message)
+    }
+  }
 
   return (
     <Modal
@@ -23,27 +36,43 @@ export default function ForgotPasswordModal({
       <View className="flex-1 mt-[35vh] items-center">
         <View className="bg-gray-700 w-5/6 p-4 rounded-lg">
           <Text className="text-white text-2xl font-bold">Forgot your password?</Text>
-          <Text className="text-white my-1">Enter your email and we'll send you a link to reset your password.</Text>
-          <View className="flex-col items-center mt-2">
-            <TextInput
-              placeholder="Email"
-              className="bg-gray-300 text-gray-600 w-4/5 rounded-xl py-3 px-3"
-              editable={true}
-              value={email}
-              onChangeText={setEmail}
-            />
-            <GradientButton
-              text='RESET'
-              onPress={onRequestClose}
-              containerClassName="w-4/5 mt-2"
-            />
-            <TouchableOpacity
-              className="rounded-xl"
-              onPress={onRequestClose}
-            >
-              <Text className="text-white font-bold text-md mt-3">Cancel</Text>
-            </TouchableOpacity>
-          </View>
+          {requestSent ? (
+            <View>
+              <Text className="text-white my-1">An email with instructions to reset your password has been sent.</Text>          
+              <TouchableOpacity
+                className="rounded-xl items-center"
+                onPress={() => {
+                  setRequestSent(false);
+                  setEmail('');
+                  onRequestClose();
+                }}
+              >
+                <Text className="text-white font-bold text-md mt-3">Close</Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View className="flex-col items-center mt-2">
+              <Text className="text-white my-1">Enter your email and we'll send you a link to reset your password.</Text>          
+              <TextInput
+                placeholder="Email"
+                className="bg-gray-300 text-gray-600 w-4/5 rounded-xl py-3 px-3"
+                editable={true}
+                value={email}
+                onChangeText={setEmail}
+              />
+              <GradientButton
+                text='RESET'
+                onPress={resetPassword}
+                containerClassName="w-4/5 mt-2"
+              />
+              <TouchableOpacity
+                className="rounded-xl"
+                onPress={onRequestClose}
+              >
+                <Text className="text-white font-bold text-md mt-3">Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         </View>
       </View>
     </Modal>
--- app/screens/profile.tsx
+++ app/screens/profile.tsx
@@ -1,12 +1,26 @@
-import { View, Text} from "react-native";
+import { View, Text, Image } from "react-native";
 import Navbar from "@components/Navbar";
+import GradientText from "../components/GradientText";
 
 
 export default function Profile() {
   return (
-    <View>
-        <Navbar />
-        <Text>Profile</Text>
-    </View>
+    <>
+      <Navbar />
+      <View className="flex-1 bg-stone-950 p-6 pt-8 justify-between">
+        <View className="w-full max-w-md items-center">
+          <Image
+            source={require('@assets/images/bonsai-logo.png')}
+            className="w-48 h-48 rounded-2xl mb-2 bg-gray-800"
+            resizeMode="contain"
+          />
+          <GradientText
+            text="First Last"
+            classStyle="text-4xl font-black mt-4"
+            size={[800, 80]}
+          />
+        </View>
+      </View>
+    </>
   );
 }
