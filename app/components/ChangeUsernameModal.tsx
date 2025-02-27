843b74cd8100e815f49f2e1803936f770eb240e5
Leon <43097991+unkn-wn@users.noreply.github.com>
2025-02-26 22:23:08 -0500

    modals styling

--- app/components/ChangeUsernameModal.tsx
+++ app/components/ChangeUsernameModal.tsx
@@ -2,6 +2,7 @@ import { Modal, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 import GradientButton from '@components/GradientButton';
 import { changeUsername } from '@components/utils/userManagement';
+import { BlurView } from 'expo-blur';
 
 interface ForgotPasswordModalProps {
   currentUsername: string;
@@ -42,53 +43,55 @@ export default function ChangeUsernameModal({
         visible={visible}
         onRequestClose={onRequestClose}
     >
-      <View className="flex-1 mt-[35vh] items-center">
-        <View className="bg-gray-700 w-5/6 p-4 rounded-lg">
-          <Text className="text-white text-2xl font-bold">Enter a New Username:</Text>
-          {requestSent ? (
-            <View>
-              <Text className="text-white my-1">Username has been changed to <Text className="font-bold">{username}</Text></Text>          
-              <TouchableOpacity
-                className="rounded-xl items-center"
-                onPress={() => {
-                  setRequestSent(false);
-                  setUsername('');
-                  setError('');
-                  onRequestClose();
-                }}
-              >
-                <Text className="text-white font-bold text-md mt-3">Close</Text>
-              </TouchableOpacity>
-            </View>
-          ) : (
-            <View className="flex-col items-center mt-2">
-              {error ? <Text className="text-red-500 mb-2">{error}</Text> : null}
-              <TextInput
-                placeholder="New username"
-                className="bg-gray-300 text-gray-600 w-4/5 rounded-xl py-3 px-3"
-                editable={true}
-                value={username}
-                onChangeText={setUsername}
-              />
-              <GradientButton
-                text='CHANGE USERNAME'
-                onPress={usernameChange}
-                containerClassName="w-4/5 mt-2"
-              />
-              <TouchableOpacity
-                className="rounded-xl"
-                onPress={() => {
+      <BlurView intensity={20} className="absolute w-full h-full">
+        <View className="flex-1 mt-[35vh] items-center">
+          <View className="bg-stone-900 w-5/6 p-4 rounded-lg">
+            <Text className="text-white text-2xl font-bold">Enter a New Username:</Text>
+            {requestSent ? (
+              <View className="w-full">
+                <Text className="text-white my-1">Username has been changed to <Text className="font-bold">{username}</Text></Text>
+                <TouchableOpacity
+                  className="rounded-xl items-center"
+                  onPress={() => {
+                    setRequestSent(false);
                     setUsername('');
                     setError('');
                     onRequestClose();
-                }}
-              >
-                <Text className="text-white font-bold text-md mt-3">Cancel</Text>
-              </TouchableOpacity>
-            </View>
-          )}
+                  }}
+                >
+                  <Text className="text-white font-bold text-md mt-3">Close</Text>
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View className="flex-col items-center mt-2 w-full">
+                {error ? <Text className="text-red-500 mb-2">{error}</Text> : null}
+                <TextInput
+                  placeholder="New username"
+                  className="bg-gray-300 text-gray-600 w-full rounded-xl py-3 px-3"
+                  editable={true}
+                  value={username}
+                  onChangeText={setUsername}
+                />
+                <GradientButton
+                  text='CHANGE USERNAME'
+                  onPress={usernameChange}
+                  containerClassName="w-full mt-2"
+                />
+                <TouchableOpacity
+                  className="rounded-xl w-full items-center"
+                  onPress={() => {
+                    setUsername('');
+                    setError('');
+                    onRequestClose();
+                  }}
+                >
+                  <Text className="text-white font-bold text-md mt-3">Cancel</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
-      </View>
+      </BlurView>
     </Modal>
   );
 }
--- app/components/ForgotPasswordModal.tsx
+++ app/components/ForgotPasswordModal.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import GradientButton from './GradientButton';
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from 'firebaseConfig';
+import { BlurView } from 'expo-blur';
 
 interface ForgotPasswordModalProps {
   visible: boolean;
@@ -34,48 +35,50 @@ export default function ForgotPasswordModal({
         visible={visible}
         onRequestClose={onRequestClose}
     >
-      <View className="flex-1 mt-[35vh] items-center">
-        <View className="bg-gray-700 w-5/6 p-4 rounded-lg">
-          <Text className="text-white text-2xl font-bold">Forgot your password?</Text>
-          {requestSent ? (
-            <View>
-              <Text className="text-white my-1">An email with instructions to reset your password has been sent.</Text>          
-              <TouchableOpacity
-                className="rounded-xl items-center"
-                onPress={() => {
-                  setRequestSent(false);
-                  setEmail('');
-                  onRequestClose();
-                }}
-              >
-                <Text className="text-white font-bold text-md mt-3">Close</Text>
-              </TouchableOpacity>
-            </View>
-          ) : (
-            <View className="flex-col items-center mt-2">
-              <Text className="text-white my-1 mb-4">Enter your email and we'll send you a link to reset your password.</Text>          
-              <TextInput
-                placeholder="Email"
-                className="bg-gray-300 text-gray-600 w-4/5 rounded-xl py-3 px-3"
-                editable={true}
-                value={email}
-                onChangeText={setEmail}
-              />
-              <GradientButton
-                text='RESET'
-                onPress={resetPassword}
-                containerClassName="w-4/5 mt-2"
-              />
-              <TouchableOpacity
-                className="rounded-xl"
-                onPress={onRequestClose}
-              >
-                <Text className="text-white font-bold text-md mt-3">Cancel</Text>
-              </TouchableOpacity>
-            </View>
-          )}
+      <BlurView intensity={20} className="absolute w-full h-full">
+        <View className="flex-1 mt-[35vh] items-center">
+          <View className="bg-stone-900 w-5/6 p-4 rounded-lg">
+            <Text className="text-white text-2xl font-bold">Forgot your password?</Text>
+            {requestSent ? (
+              <View className="w-full">
+                <Text className="text-white my-1">An email with instructions to reset your password has been sent.</Text>
+                <TouchableOpacity
+                  className="rounded-xl items-center"
+                  onPress={() => {
+                    setRequestSent(false);
+                    setEmail('');
+                    onRequestClose();
+                  }}
+                >
+                  <Text className="text-white font-bold text-md mt-3">Close</Text>
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View className="flex-col items-center mt-2 w-full">
+                <Text className="text-white my-1 mb-4">Enter your email and we'll send you a link to reset your password.</Text>
+                <TextInput
+                  placeholder="Email"
+                  className="bg-gray-300 text-gray-600 w-full rounded-xl py-3 px-3"
+                  editable={true}
+                  value={email}
+                  onChangeText={setEmail}
+                />
+                <GradientButton
+                  text='RESET'
+                  onPress={resetPassword}
+                  containerClassName="w-full mt-2"
+                />
+                <TouchableOpacity
+                  className="rounded-xl w-full items-center"
+                  onPress={onRequestClose}
+                >
+                  <Text className="text-white font-bold text-md mt-3">Cancel</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
-      </View>
+      </BlurView>
     </Modal>
   );
 }
