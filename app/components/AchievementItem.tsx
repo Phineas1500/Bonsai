4ac20128c24c602321448b3cbc0815088bbc3a16
Caleb Chua <112199132+calebchua@users.noreply.github.com>
2025-05-01 03:26:06 -0400

    Achievement Icons

--- app/components/AchievementItem.tsx
+++ app/components/AchievementItem.tsx
@@ -14,7 +14,7 @@ export default function AchievementItem({
   return (
     <View className={`bg-[#1D1D1D] h-34 w-32 rounded-2xl items-center p-2 ${classStyle}`}>
       <Image
-        source={{ uri: url }}
+        source={{ uri: `https://api.dicebear.com/9.x/shapes/png?seed=${title}` }}
         className="h-12 w-12 rounded-full object-cover bg-white mb-1"
         resizeMode="contain"
       />
